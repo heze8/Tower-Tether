03cd9ba5eb5801391e42c9834d08c1954c407488
Zaiah <zaiah0505@hotmail.com>
2021-06-13 14:57:28 +0800

    Removed options and added main scene load.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        //TODO: Load the game scene
+        SceneManager.LoadScene(1);
     }
     
     public void QuitGame()
